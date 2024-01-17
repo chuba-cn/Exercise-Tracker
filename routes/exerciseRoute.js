f45bdf7af9669278ca09bc146f91006b317e258d
Chuba . C <chinemelumchubanwene57@gmail.com>
2024-01-17 15:44:57 +0100

    add controllers for routes

--- routes/exerciseRoute.js
+++ routes/exerciseRoute.js
@@ -1,40 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user');
-const Exercise = require('../models/exercise');
+const exercise = require('../controllers/exercise');
 
-router.post('/:id/exercises', async(req, res) =>{
-    try{
-        const {id} = req.params;
-        const {description, duration} = req.body;
-        const dateInput = req.body.date || Date.now();
-        const dateObject = new Date(dateInput);
-
-        if(dateObject === 'Invalid Date'){
-            return res.status(400).json({error: 'Invalid Date'});
-        }
-
-        const user = await User.findById(id);
-        
-        if(user){
-            const newExercise = new Exercise({ description, date: dateObject, duration });
-            newExercise.author = id;
-            newExercise.save();
-
-            const response = {
-              username: user.username,
-              description: newExercise.description,
-              duration: newExercise.duration,
-              date: newExercise.date.toDateString(),
-              _id: id
-            };
-            res.status(200).json(response);
-        }else{res.status(400).json({error: 'No user found with given ID!!'})};
-
-    }catch(error){
-        console.error(error);
-        res.status(500).json({error: error.message});
-    } 
-});
+router.post('/:id/exercises', exercise.createExercise);
 
 module.exports = router;
